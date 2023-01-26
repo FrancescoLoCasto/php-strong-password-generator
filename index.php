b42d882c10dc8a1d703ff179a7dbb63a72bc23fc
MyUsername <my@email.com>
2023-01-26 17:48:32 +0100

    style implement

--- index.php
+++ index.php
@@ -12,21 +12,21 @@
       <!-- Link bootstrap -->
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
    </head>
-   <body>
-      
-
-
-   <form action="./index.php" method="GET">
-      <label for="userInput">Inserisci la lungezza della nuova password</label>
-      <input type="number" name="userInput">
-      <button type="submit">Genera Password</button>
-      <h1>
-         <?php echo randomGeneratorPassword(); ?>
-      </h1>
-   </form>
-
-
-
-
-   </body>
+      <body>
+            
+         <div class="container">
+            <div class="row">
+               <div class="col-12 text-center">
+                  <form action="./index.php" method="GET">
+                     <label for="userInput">Inserisci la lungezza della nuova password</label>
+                     <input type="number" name="userInput" class="m-2">
+                     <button type="submit" class="btn btn-primary">Genera Password</button>
+                     <h1>
+                        <?php echo randomGeneratorPassword(); ?>
+                     </h1>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </body>
 </html>
